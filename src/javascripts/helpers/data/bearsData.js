5ec27573c8d4e5d5208d5a1958b18ba86d22bc6a
John-Ryan-Johnson <john4johnkids@gmail.com>
2020-02-27 13:53:23 -0600

    bear cards printing

---
+++ src/javascripts/helpers/data/bearsData.js
@@ -0,0 +1,25 @@
+const bears = [
+  {
+    id: 'bear1',
+    name: 'Brutus',
+    imageUrl: 'https://www.publicdomainpictures.net/pictures/230000/nahled/brown-bear-1504708649cqE.jpg',
+    attempts: 2,
+    success: 0,
+  },
+  {
+    id: 'bear2',
+    name: 'Goldie',
+    imageUrl: 'https://www.publicdomainpictures.net/pictures/220000/nahled/kodiak-brown-bear.jpg',
+    attempts: 2,
+    success: 1,
+  },
+  {
+    id: 'bear3',
+    name: 'Tike',
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/3/3a/Blond_juvenile_NM_brown_bear_among_pinyons%2C_detail.jpg',
+    attempts: 3,
+    success: 1,
+  },
+];
+
+export default { bears };
