c046c4d18d2b3f0c8f9f2689ed09323abb4f213a
John-Ryan-Johnson <john4johnkids@gmail.com>
2020-02-29 13:25:16 -0600

    added buttons and alert and styling

--- src/javascripts/components/river/river.js
+++ src/javascripts/components/river/river.js
@@ -1,23 +1,32 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-alert */
+import $ from 'jquery';
 import utilities from '../../helpers/utilities';
 
-import bearsData from '../../helpers/data/bearsData';
+import bearData from '../../helpers/data/bearData';
 
 const printBears = () => {
   let domString = '';
-  bearsData.bears.forEach((bear) => {
+  bearData.bears.forEach((bear) => {
     domString += `
-      <div class="col-3 mt-5">
-        <div class="card text-center">
-          <img src="${bear.imageUrl}" class="card-img-top" alt="bear pic">
-          <div class="card-body">
-            <h4 class="card-title">${bear.name}</h4>
-            <p class="card-text"></p>
-          </div>
-        </div>
-      </div>
+    <div class="card mx-5 mt-5" style="max-width: 18rem;">
+    <img class="card-img-top" src="${bear.imageUrl}" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title text-center">${bear.name}</h5><hr>
+      <h6 class="text-center mb-3">Fishing Prowess</h6>
+      <button class="failed btn btn-primary ml-3">Attempt</button>
+      <button class=" booYaa btn btn-success ml-5">Success</button>
+    </div>
+  </div>
     `;
   });
   utilities.printToDom('bear-container', domString);
+  $('.failed').click(() => {
+    alert('Failed Attempt !');
+  });
+  $('.booYaa').click(() => {
+    alert('Caught It !');
+  });
 };
 
 export default { printBears };
--- src/javascripts/helpers/data/bearData.js
+++ src/javascripts/helpers/data/bearData.js
@@ -4,21 +4,21 @@ const bears = [
     name: 'Brutus',
     imageUrl: 'https://www.publicdomainpictures.net/pictures/230000/nahled/brown-bear-1504708649cqE.jpg',
     attempts: 2,
-    success: 0,
+    successes: 0,
   },
   {
     id: 'bear2',
     name: 'Goldie',
     imageUrl: 'https://www.publicdomainpictures.net/pictures/220000/nahled/kodiak-brown-bear.jpg',
     attempts: 2,
-    success: 1,
+    successes: 1,
   },
   {
     id: 'bear3',
     name: 'Tike',
     imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/3/3a/Blond_juvenile_NM_brown_bear_among_pinyons%2C_detail.jpg',
     attempts: 3,
-    success: 1,
+    successes: 1,
   },
 ];
 
