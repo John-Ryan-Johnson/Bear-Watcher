5f9dd4b7fd2194e0859226e74c290cadf36e8c93
John-Ryan-Johnson <john4johnkids@gmail.com>
2020-03-01 18:13:20 -0600

    fishing buttons console.log and attempts print to the dom

--- src/javascripts/components/river/river.js
+++ src/javascripts/components/river/river.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-undef */
-/* eslint-disable no-alert */
 import $ from 'jquery';
 import utilities from '../../helpers/utilities';
 
@@ -7,7 +5,8 @@ import bearData from '../../helpers/data/bearData';
 
 const printBears = () => {
   let domString = '';
-  bearData.bears.forEach((bear) => {
+  const bears = bearData.getBears();
+  bears.forEach((bear) => {
     domString += `
     <div class="card mx-5 mt-5" style="max-width: 18rem;">
     <img class="card-img-top" src="${bear.imageUrl}" alt="Card image cap">
@@ -15,17 +14,31 @@ const printBears = () => {
       <h5 class="card-title text-center">${bear.name}</h5><hr>
       <h6 class="text-center mb-3">Fishing Prowess</h6>
       <button class="failed btn btn-primary ml-3">Attempt</button>
-      <button class=" booYaa btn btn-success ml-5">Success</button>
+      <button class="booYaa btn btn-success ml-5">Success</button>
+      <div class="fa mt-3">
+        <p class="attempts">Failed Attempts:</p>
+        <input type="text" id="attempts" value="${bear.attempts}">
+      </div>
+      <div class="sa">
+        <p>Successful Attempts:</p>
+        <input type="text" id="success" value="${bear.successes}">
+      </div>
+      <div class="fc">
+        <p>Fish Caught:</p>
+        <input type="text" id="total" value="${bear.successes}">
+      </div>
     </div>
   </div>
     `;
   });
   utilities.printToDom('bear-container', domString);
-  $('.failed').click(() => {
-    alert('Failed Attempt !');
+
+  $(document).on('click', '.failed', () => {
+    console.error('Failed attempt!!');
   });
-  $('.booYaa').click(() => {
-    alert('Caught It !');
+
+  $(document).on('click', '.booYaa', () => {
+    console.error('Success!!');
   });
 };
 
